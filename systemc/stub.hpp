4cbfe216853265774b0178dae719e175f3cad2b3
Chris <amstutz@kit.edu>
2014-01-10 08:40:42 +0100

    Add data format for stub

---
+++ systemc/stub.hpp
@@ -0,0 +1,23 @@
+/*!
+ * @file stub.hpp
+ * @author Christian Amstutz
+ * @date Jan 10, 2014
+ *
+ * @brief
+ *
+ */
+
+/*
+ *  Copyright (c) 2014 by Christian Amstutz
+ */
+
+#pragma once
+
+
+/*!
+ * @brief
+ */
+struct stub {
+  int address;
+  int bend;
+};
