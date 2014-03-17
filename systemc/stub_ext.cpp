1545753fe63b4be74ca2190b400f194e9e064d6c
Chris <amstutz@kit.edu>
2014-03-17 10:47:43 +0100

    fix bug and code cosmetics in stub_ext

--- systemc/stub_ext.cpp
+++ systemc/stub_ext.cpp
@@ -1,7 +1,7 @@
 /*!
  * @file stub_ext.cpp
  * @author Christian Amstutz
- * @date Feb 6, 2014
+ * @date Mar 17, 2014
  *
  * @brief
  *
@@ -14,123 +14,124 @@
 #include "stub_ext.hpp"
 
 // *****************************************************************************
-stub_ext::stub_ext() {
-  set_phi_coord(0);
-  set_z_coord(0);
-  set_fechip_nr(0);
-  set_strip_nr(0);
+stub_ext::stub_ext()
+{
+    set_phi_coord(0);
+    set_z_coord(0);
+    set_fechip_nr(0);
+    set_strip_nr(0);
 
-  return;
+    return;
 }
 
 // *****************************************************************************
-inline void stub_ext::set_phi_coord(const phi_coord_t phi_coord) {
+inline void stub_ext::set_phi_coord(const phi_coord_t phi_coord)
+{
+    this->phi_coord = phi_coord;
 
-  this->phi_coord = phi_coord;
-
-  return;
+    return;
 }
 
 // *****************************************************************************
-inline stub_ext::phi_coord_t stub_ext::get_phi_coord() const {
-
-  return phi_coord;
+inline stub_ext::phi_coord_t stub_ext::get_phi_coord() const
+{
+    return (phi_coord);
 }
 
 // *****************************************************************************
-inline void stub_ext::set_z_coord(const z_coord_t z_coord) {
+inline void stub_ext::set_z_coord(const z_coord_t z_coord)
+{
+    this->z_coord = z_coord;
 
-  this->z_coord = z_coord;
-
-  return;
+    return;
 }
 
 // *****************************************************************************
-inline stub_ext::z_coord_t stub_ext::get_z_coord() const {
-
-  return z_coord;
+inline stub_ext::z_coord_t stub_ext::get_z_coord() const
+{
+    return (z_coord);
 }
 
 // *****************************************************************************
-inline void stub_ext::set_fechip_nr(const fechip_nr_t address) {
+inline void stub_ext::set_fechip_nr(const fechip_nr_t address)
+{
+    this->fechip_nr = address;
 
-  this->fechip_nr = fechip_nr;
-
-  return;
+    return;
 }
 
 // *****************************************************************************
-inline stub_ext::fechip_nr_t stub_ext::get_fechip_nr() const {
-
-  return fechip_nr;
+inline stub_ext::fechip_nr_t stub_ext::get_fechip_nr() const
+{
+    return (fechip_nr);
 }
 
 // *****************************************************************************
-inline void stub_ext::set_strip_nr(const strip_nr_t address) {
+inline void stub_ext::set_strip_nr(const strip_nr_t address)
+{
+    this->strip_nr = address;
 
-  this->strip_nr = strip_nr;
-
-  return;
+    return;
 }
 
 // *****************************************************************************
-inline stub_ext::strip_nr_t stub_ext::get_strip_nr() const {
-
-  return strip_nr;
+inline stub_ext::strip_nr_t stub_ext::get_strip_nr() const
+{
+  return (strip_nr);
 }
 
 // *****************************************************************************
-sc_bv<stub_ext::stub_width> stub_ext::getBitVector() const {
+sc_bv<stub_ext::stub_width> stub_ext::getBitVector() const
+{
+    sc_bv<stub_width> full_stub;
+    full_stub = ( (phi_coord, z_coord, fechip_nr, strip_nr) );
 
-  sc_bv<stub_width> full_stub;
-  full_stub = ( (phi_coord, z_coord, fechip_nr, strip_nr) );
-
-  return full_stub;
+    return (full_stub);
 }
 
 // *****************************************************************************
-bool stub_ext::operator == (const stub_ext &rhs) const {
+bool stub_ext::operator == (const stub_ext &rhs) const
+{
+    bool equality = true;
+    equality = equality && (rhs.phi_coord == phi_coord);
+    equality = equality && (rhs.z_coord == z_coord);
+    equality = equality && (rhs.fechip_nr == fechip_nr);
+    equality = equality && (rhs.strip_nr == strip_nr);
 
-  bool equality = true;
-  equality = equality && (rhs.phi_coord == phi_coord);
-  equality = equality && (rhs.z_coord == z_coord);
-  equality = equality && (rhs.fechip_nr == fechip_nr);
-  equality = equality && (rhs.strip_nr == strip_nr);
-
-  return equality;
+    return (equality);
 }
 
 // *****************************************************************************
-stub_ext& stub_ext::operator = (const stub_ext & rhs) {
+stub_ext& stub_ext::operator = (const stub_ext & rhs)
+{
+    phi_coord = rhs.phi_coord;
+    z_coord = rhs.z_coord;
+    fechip_nr = rhs.fechip_nr;
+    strip_nr = rhs.strip_nr;
 
-  phi_coord = rhs.phi_coord;
-  z_coord = rhs.z_coord;
-  fechip_nr = rhs.fechip_nr;
-  strip_nr = rhs.strip_nr;
-
-  return *this;
+    return (*this);
 }
 
 // *****************************************************************************
-ostream& operator << (ostream &os, stub_ext const &v) {
+ostream& operator << (ostream &os, stub_ext const &v)
+{
+    os << "[phi=" << std::hex
+            << v.phi_coord.to_uint() << ",Z="
+            << v.z_coord.to_uint() << ",FE="
+            << v.fechip_nr.to_uint() << ",str="
+            << v.strip_nr.to_uint()
+            << "]";
 
-  os << "[phi=" << std::hex
-      << v.phi_coord.to_uint() << ",Z="
-      << v.z_coord.to_uint() << ",FE="
-      << v.fechip_nr.to_uint() << ",str="
-      << v.strip_nr.to_uint()
-      << "]";
-
-  return os;
+    return (os);
 }
 
 // *****************************************************************************
-void sc_trace(sc_trace_file *tf, const stub_ext &v, const std::string &name) {
+void sc_trace(sc_trace_file *tf, const stub_ext &v, const std::string &name)
+{
+    sc_trace(tf, v.phi_coord, name + ".phi_coord");
+    sc_trace(tf, v.z_coord, name + ".z_coord");
+    sc_trace(tf, v.fechip_nr, name + ".fechip_nr");
+    sc_trace(tf, v.strip_nr, name + ".strip_nr");
 
-  sc_trace(tf, v.phi_coord, name + ".phi_coord");
-  sc_trace(tf, v.z_coord, name + ".z_coord");
-  sc_trace(tf, v.fechip_nr, name + ".fechip_nr");
-  sc_trace(tf, v.strip_nr, name + ".strip_nr");
-
-  return;
+    return;
 }
