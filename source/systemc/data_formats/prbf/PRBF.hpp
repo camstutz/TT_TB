1a84e1b47d38c8354106dcb758be29dd804d17d1
Christian Amstutz <amstutz@kit.edu>
2015-04-01 14:43:34 +0200

    add central header file with PRBF.0 definition

---
+++ source/systemc/data_formats/prbf/PRBF.hpp
@@ -0,0 +1,19 @@
+/*!
+ * @file PRBF.hpp
+ * @author Christian Amstutz
+ * @date April 1, 2015
+ *
+ * @brief
+ *
+ */
+
+/*
+ *  Copyright (c) 2015 by Christian Amstutz
+ */
+
+#pragma once
+
+#include "PRBF_frame.hpp"
+#include "stub_PRBF0.hpp"
+
+typedef PRBF_frame<stub_PRBF0> PRBF_0;
