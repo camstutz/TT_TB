d03780816dc8c7ebff602d67fd3ac6e5ab09e03d
Christian Amstutz <amstutz@kit.edu>
2015-04-01 12:10:25 +0200

    add basic skeleton of PRBF

---
+++ source/systemc/data_formats/prbf/PRBF.cpp
@@ -0,0 +1,20 @@
+/*!
+ * @file PRBF.cpp
+ * @author Christian Amstutz
+ * @date April 1, 2015
+ *
+ * @brief
+ *
+ */
+
+/*
+ *  Copyright (c) 2015 by Christian Amstutz
+ */
+
+#include "PRBF.hpp"
+
+// *****************************************************************************
+PRBF::PRBF()
+{
+    return;
+}
---
+++ source/systemc/data_formats/prbf/PRBF.hpp
@@ -0,0 +1,24 @@
+/*!
+ * @file stub_PRBF.hpp
+ * @author Christian Amstutz
+ * @date April 1, 2015
+ *
+ * @brief
+ *
+ */
+
+/*
+ *  Copyright (c) 2015 by Christian Amstutz
+ */
+
+#pragma once
+
+#include "stub_PRBF.hpp"
+
+// *****************************************************************************
+class PRBF
+{
+public:
+    PRBF();
+private:
+};
