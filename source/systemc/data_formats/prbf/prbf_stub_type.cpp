3b55e5a64afc0c96e2a28285189afbfffa50a1e9
Christian Amstutz <amstutz@kit.edu>
2015-04-01 11:05:42 +0200

    add very basic version of base class stub_type

---
+++ source/systemc/data_formats/prbf/prbf_stub_type.cpp
@@ -0,0 +1,20 @@
+/*!
+ * @file prbf_stub_type.cpp
+ * @author Christian Amstutz
+ * @date April 1, 2015
+ *
+ * @brief
+ *
+ */
+
+/*
+ *  Copyright (c) 2015 by Christian Amstutz
+ */
+
+#include "prbf_stub_type.hpp"
+
+// *****************************************************************************
+prbf_stub_type::prbf_stub_type()
+{
+    return;
+}
---
+++ source/systemc/data_formats/prbf/prbf_stub_type.hpp
@@ -0,0 +1,25 @@
+/*!
+ * @file prbf_stub_type.hpp
+ * @author Christian Amstutz
+ * @date April 1, 2015
+ *
+ * @brief
+ *
+ */
+
+/*
+ *  Copyright (c) 2015 by Christian Amstutz
+ */
+
+#pragma once
+
+// *****************************************************************************
+class prbf_stub_type
+{
+public:
+    //typedef stub_type stub_t;
+
+    prbf_stub_type();
+
+private:
+};
