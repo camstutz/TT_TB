88d81386a6fc66f7b6c556a1d8c9210e0e5f80e5
Christian Amstutz <amstutz@kit.edu>
2015-04-28 16:18:56 +0200

    Squashed 'source/systemc/libraries/systemc_helpers/' content from commit d74202c
    git-subtree-dir: source/systemc/libraries/systemc_helpers
    git-subtree-split: d74202c9983d8e7e78ede379416d8d8409245429

---
+++ sc_map/sc_map_base.hpp
@@ -0,0 +1,263 @@
+/*!
+ * @file sc_map_base.hpp
+ * @author Christian Amstutz
+ * @date June 4, 2014
+ *
+ * @brief
+ *
+ */
+
+/*
+ *  Copyright (c) 2014 by Christian Amstutz
+ */
+
+#pragma once
+
+#include <cstddef>
+#include <string>
+#include <sstream>
+
+#include <systemc.h>
+
+#include "sc_map_iter_sequential.hpp"
+#include "../modelsim_support/modelsim_support.hpp"
+
+//* todo: add access operators [] .at()
+
+//******************************************************************************
+template<typename object_type>
+class sc_map_base : public sc_object
+{
+public:
+    typedef std::vector<object_type*> container_type;
+    typedef typename container_type::iterator container_iterator;
+    typedef typename container_type::const_iterator const_container_iterator;
+    typedef int key_type;
+    typedef sc_map_iter_sequential<object_type> iterator;
+    typedef ptrdiff_t difference_type;
+    typedef typename container_type::size_type size_type;
+    typedef object_type value_type;
+    typedef object_type* pointer;
+    typedef object_type& reference;
+
+    //* todo: hide objects to the outside world
+    container_type objects;
+
+    sc_map_base(const sc_module_name name);
+    virtual ~sc_map_base() {};
+
+    // todo: add simple init with just number
+    //void init( size_type n )
+    //    { init( n, &sc_map_base<object_type>::create_element ); }
+
+    template<typename Creator>
+    void init(size_type n, Creator object_creator);
+
+    size_type size();
+    iterator begin();
+    iterator end();
+
+    template<typename signal_type>
+    void bind(sc_map_base<signal_type> signal_map);
+    // todo: how to access members of modules over which is iterated
+    // todo: single signal to many port binding
+    template<typename signal_type>
+    bool bind_by_iter(sc_map_iterator<object_type>& port_iter, sc_map_iterator<signal_type>& signal_iter);
+    template<typename signal_type>
+    bool bind_by_iter(sc_map_iterator<signal_type>& signal_iter);
+
+    void make_sensitive(sc_sensitive& sensitive_list) const;
+    template<typename data_type>
+    void write_all(const data_type& value);
+    //template<typename data_type>
+    // todo: void write_range(data_type value);
+
+    //* todo: add const to second argument of sc_trace
+    template<typename trace_obj_type>
+    friend void sc_trace(sc_trace_file* tf, sc_map_base<trace_obj_type>& sc_map, const std::string& name);
+
+    /** Function for tracing support in ModelSim */
+#ifdef MODELSIM_COMPILER
+    template<typename data_type>
+    void register_signal_modelsim();
+#endif
+
+    friend class sc_map_iterator<object_type>;
+};
+
+//******************************************************************************
+
+//******************************************************************************
+template<typename object_type>
+sc_map_base<object_type>::sc_map_base(const sc_module_name name) :
+        sc_object(name)
+{}
+
+//******************************************************************************
+template<typename object_type>
+template<typename Creator>
+void sc_map_base<object_type>::init(size_type n, Creator object_creator)
+{
+    // check correct call conditions
+//    if (!n)
+//      return false;
+//
+//    if( size() ) // already filled
+//    {
+//      std::stringstream str;
+//      str << name()
+//          << ", size=" << size()
+//          << ", requested size=" << n;
+//      SC_REPORT_ERROR( SC_ID_VECTOR_INIT_CALLED_TWICE_
+//                     , str.str().c_str() );
+//      return false;
+
+    objects.reserve(n);
+    for (size_type i = 0; i<n; ++i)
+    {
+        std::string name = basename();
+        const char* cname = name.c_str();
+
+        object_type* p = object_creator(cname, i);
+        objects.push_back(p);
+    }
+
+    return;
+}
+
+//******************************************************************************
+template<typename object_type>
+typename sc_map_base<object_type>::size_type sc_map_base<object_type>::size()
+{
+    return (objects.size());
+}
+
+//******************************************************************************
+template<typename object_type>
+typename sc_map_base<object_type>::iterator sc_map_base<object_type>::begin()
+{
+    return (iterator(*this, 0));
+}
+
+//******************************************************************************
+template<typename object_type>
+typename sc_map_base<object_type>::iterator sc_map_base<object_type>::end()
+{
+    return (iterator(*this, objects.size()));
+}
+
+//******************************************************************************
+template<typename object_type>
+template<typename signal_type>
+void sc_map_base<object_type>::bind(sc_map_base<signal_type> signal_map)
+{
+    // todo: check range
+
+    iterator port_it = begin();
+    iterator port_end = end();
+    typename sc_map_base<signal_type>::iterator signal_it = signal_map.begin();
+    typename sc_map_base<signal_type>::iterator signal_end = signal_map.end();
+
+    for (; port_it != port_end; ++port_it)
+    {
+        port_it->bind(*signal_it);
+        ++signal_it;
+    }
+
+    return;
+}
+
+//******************************************************************************
+template<typename object_type>
+template<typename signal_type>
+bool sc_map_base<object_type>::bind_by_iter(sc_map_iterator<object_type>& port_iter,
+        sc_map_iterator<signal_type>& signal_iter)
+{
+    // todo: check for equal size
+    // todo: check for same object
+    // todo: check for compatibility of port and signal (pre-processor)
+
+    iterator end = this->end();
+    for ( ; port_iter != end; ++port_iter)
+    {
+        port_iter->bind(*signal_iter);
+        ++signal_iter;
+    }
+
+    return (true);
+}
+
+//******************************************************************************
+template<typename object_type>
+template<typename signal_type>
+bool sc_map_base<object_type>::bind_by_iter(sc_map_iterator<signal_type>& signal_iter)
+{
+    iterator port_iter = this->begin();
+    bool bind_ok = bind_by_iter(port_iter, signal_iter);
+
+    return (bind_ok);
+}
+
+//******************************************************************************
+template<typename object_type>
+void sc_map_base<object_type>::make_sensitive(
+        sc_sensitive& sensitive_list) const
+{
+    // todo: change it to a traditional way of sensitivity lists
+    // todo: make it possible to react on other than pos
+    // todo: make it not to use the reference, but const
+    const_container_iterator object_it = objects.begin();
+    for (; object_it != objects.end(); ++object_it)
+    {
+        sensitive_list << **object_it;
+    }
+
+    return;
+}
+
+//******************************************************************************
+template<typename object_type>
+template<typename data_type>
+void sc_map_base<object_type>::write_all(const data_type& value)
+{
+    container_iterator object_it = objects.begin();
+    for(; object_it != objects.end(); ++object_it)
+    {
+        (*object_it)->write(value);
+    }
+
+    return;
+}
+
+//******************************************************************************
+template<typename trace_obj_type>
+void sc_trace(sc_trace_file* tf, sc_map_base<trace_obj_type>& sc_map, const std::string& name)
+{
+    typename sc_map_base<trace_obj_type>::iterator object_it = sc_map.begin();
+    for (; object_it != sc_map.end(); ++object_it)
+    {
+        std::stringstream full_name;
+        full_name << name << "." << object_it->name();
+        sc_trace(tf, *object_it, full_name.str().c_str());
+    }
+
+    return;
+}
+
+// *****************************************************************************
+#ifdef MODELSIM_COMPILER
+
+template <typename object_type>
+template <typename data_type>
+void sc_map_base<object_type>::register_signal_modelsim()
+{
+    const_container_iterator object_it = objects.begin();
+    for (; object_it != objects.end(); ++object_it)
+    {
+        SC_MTI_REGISTER_CUSTOM_DEBUG(*object_it, data_type::get_max_value_length(), data_type::mti_debug_cb);
+    }
+    
+    return;
+}
+
+#endif
---
+++ sc_map/sc_map_square.hpp
@@ -0,0 +1,274 @@
+/*!
+ * @file sc_map_square.hpp
+ * @author Christian Amstutz
+ * @date December 10, 2014
+ *
+ * @brief
+ *
+ */
+
+/*
+ *  Copyright (c) 2014 by Christian Amstutz
+ */
+
+#pragma once
+
+#include <string>
+#include <sstream>
+#include <map>
+#include <utility>
+
+#include <systemc.h>
+
+#include "sc_map_base.hpp"
+#include "sc_map_iter_square.hpp"
+
+//******************************************************************************
+template<typename object_type>
+class sc_map_square : public sc_map_base<object_type>
+{
+public:
+    typedef sc_map_iter_square<object_type> square_iterator;
+    typedef typename sc_map_base<object_type>::key_type key_type;
+    typedef struct
+    {
+        key_type Y_dim;
+        key_type X_dim;
+    } full_key_type;
+    typedef typename sc_map_base<object_type>::size_type size_type;
+    typedef std::map<key_type, size_type> map_1d_type;
+    typedef std::map<key_type, map_1d_type> map_type;
+
+    static const key_type default_start_id_Y = 0;
+    static const key_type default_start_id_X = 0;
+
+    sc_map_square(const size_type element_cnt_Y,
+            const size_type element_cnt_X, const sc_module_name name = "",
+            const key_type start_id_Y = default_start_id_Y,
+            const key_type start_id_X = default_start_id_X);
+
+    size_type size_Y() const;
+    size_type size_X() const;
+
+    object_type& at(const key_type key_Y, const key_type key_X);
+    std::pair<bool, full_key_type> get_key(const object_type& object) const;
+
+    square_iterator begin_partial(const key_type pos_Y, const bool iterate_Y,
+                                  const key_type pos_X, const bool iterate_X);
+    square_iterator begin_partial(
+            const key_type start_Y, const key_type stop_Y, const bool iterate_Y,
+            const key_type start_X, const key_type stop_X, const bool iterate_X);
+
+    template<typename signal_type>
+    bool bind(sc_map_square<signal_type>& signals_map);
+
+private:
+    const key_type start_id_Y;
+    const key_type start_id_X;
+
+    map_type objects_map;
+
+    size_type get_vect_pos(key_type pos_Y, key_type pos_X);
+
+    class creator
+    {
+    public:
+        size_type size_Y;
+        size_type size_X;
+
+        creator(const size_type size_Y, const size_type size_X);
+        object_type* operator() (const sc_module_name name,
+                sc_map_square<object_type>::key_type id);
+    };
+
+    friend class sc_map_iter_square<object_type>;
+};
+
+//******************************************************************************
+
+//******************************************************************************
+template<typename object_type>
+sc_map_square<object_type>::sc_map_square(
+        const size_type element_cnt_Y, const size_type element_cnt_X,
+        const sc_module_name name, const key_type start_id_Y,
+        const key_type start_id_X) :
+        sc_map_base<object_type>(name),
+        start_id_Y(start_id_Y),
+        start_id_X(start_id_X)
+{
+    size_type element_count = element_cnt_X * element_cnt_Y;
+    this->init(element_count, creator(element_cnt_Y, element_cnt_X));
+
+    for (size_type y = 0; y<element_cnt_Y; ++y) {
+        for (size_type x = 0; x<element_cnt_X; ++x) {
+            key_type vector_id = y*element_cnt_X + x;
+            objects_map[start_id_Y+y][start_id_X+x] = vector_id;
+        }
+    }
+
+    return;
+}
+
+//******************************************************************************
+template<typename object_type>
+typename sc_map_square<object_type>::size_type
+        sc_map_square<object_type>::size_Y() const
+{
+    return (objects_map.size());
+}
+
+//******************************************************************************
+template<typename object_type>
+typename sc_map_square<object_type>::size_type
+        sc_map_square<object_type>::size_X() const
+{
+    typename map_type::const_iterator first_Y = objects_map.begin();
+    return (first_Y->second.size() );
+}
+
+//******************************************************************************
+template<typename object_type>
+object_type& sc_map_square<object_type>::at(const key_type key_Y,
+        const key_type key_X)
+{
+    object_type& ret_object = *(this->objects[get_vect_pos(key_Y, key_X)]);
+
+    return ret_object;
+}
+
+//******************************************************************************
+template<typename object_type>
+std::pair<bool, typename sc_map_square<object_type>::full_key_type>
+        sc_map_square<object_type>::get_key(const object_type& object) const
+{
+    std::pair<bool, full_key_type> full_key;
+    full_key.first = false;
+
+    typename map_type::const_iterator linear_part_it;
+    linear_part_it = objects_map.begin();
+    for (; linear_part_it != objects_map.end(); ++linear_part_it)
+    {
+        typename map_1d_type::const_iterator element_it;
+        element_it = linear_part_it->second.begin();
+        for (; element_it != linear_part_it->second.end(); ++element_it)
+        {
+            const object_type* map_object = this->objects[element_it->second];
+            if (map_object == &object)
+            {
+                full_key.first = true;
+                full_key.second.Y_dim = linear_part_it->first;
+                full_key.second.X_dim = element_it->first;
+                break;
+            }
+        }
+    }
+
+    return (full_key);
+}
+
+//******************************************************************************
+template<typename object_type>
+typename sc_map_square<object_type>::square_iterator
+        sc_map_square<object_type>::begin_partial(
+        const key_type pos_Y, const bool iterate_Y,
+        const key_type pos_X, const bool iterate_X)
+{
+    key_type start_Y, stop_Y, start_X, stop_X;
+
+    if (iterate_Y)
+    {
+        start_Y = start_id_Y;
+        stop_Y = start_id_Y+size_Y()-1;
+    }
+    else
+    {
+        start_Y = pos_Y;
+        stop_Y = pos_Y;
+    }
+
+    if (iterate_X)
+    {
+        start_X = start_id_X;
+        stop_X = start_id_X+size_X()-1;
+    }
+    else
+    {
+        start_X = pos_X;
+        stop_X = pos_X;
+    }
+
+    sc_map_iter_square<object_type> square_map_it(*this,
+            start_Y, stop_Y, iterate_Y,
+            start_X, stop_X, iterate_X);
+    return (square_map_it);
+}
+
+//******************************************************************************
+template<typename object_type>
+typename sc_map_square<object_type>::square_iterator
+        sc_map_square<object_type>::begin_partial(
+        const key_type start_Y, const key_type stop_Y, const bool iterate_Y,
+        const key_type start_X, const key_type stop_X, const bool iterate_X)
+{
+    sc_map_iter_square<object_type> square_map_it(*this,
+            start_Y, stop_Y, iterate_Y,
+            start_X, stop_X, iterate_X);
+    return (square_map_it);
+}
+
+//******************************************************************************
+template<typename object_type>
+template<typename signal_type>
+bool sc_map_square<object_type>::bind(sc_map_square<signal_type>& signals_map)
+{
+    if ( (this->size_Y() !=  signals_map.size_Y()) &
+            (this->size_X() !=  signals_map.size_X()) )
+    {
+        std::cout << "Error: Binding of port with signal of different dimension."
+                << std::endl;
+        return(false);
+    }
+
+    sc_map_base<object_type>::bind(signals_map);
+
+    return (true);
+}
+
+//******************************************************************************
+template<typename object_type>
+typename sc_map_square<object_type>::size_type
+        sc_map_square<object_type>::get_vect_pos(key_type pos_Y, key_type pos_X)
+{
+    // todo: at exception handling for out of range accesses
+    size_type vector_pos = objects_map.at(pos_Y).at(pos_X);
+
+    return vector_pos;
+}
+
+//******************************************************************************
+template<typename object_type>
+sc_map_square<object_type>::creator::creator(
+        const sc_map_square<object_type>::size_type size_Y,
+        const sc_map_square<object_type>::size_type size_X) :
+        size_Y(size_Y), size_X(size_X)
+{}
+
+//******************************************************************************
+template<typename object_type>
+object_type* sc_map_square<object_type>::creator::operator() (
+        const sc_module_name name, sc_map_square<object_type>::key_type id)
+{
+    // todo: integrate a label for the dimensions
+
+    sc_map_square<object_type>::key_type id_X = id % size_X;
+    sc_map_square<object_type>::key_type id_Y = id / size_X;
+
+    // todo: include the start index to the naming
+
+    std::stringstream full_name;
+
+    full_name << name << "-" << id_Y << "_" << id_X;
+
+
+    return (new object_type(full_name.str().c_str()));
+}
